ce60ccc4b4f8548e4e34932c5c20d5092b9b92f9
Dinesh Arcot Jayachandran <Dinesh_A03@ad.infosys.com>
2020-07-08 17:13:17 +0200

    Updates

---
+++ precache-manifest.d38e286d6d241e1beecce88fb447b686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3e1782df394a0936d48a6a354fd854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dedbc7e0a3d529d1f54",
+    "url": "/static/js/2.e42154bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e42154bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def2153bfdce573e8e44",
+    "url": "/static/js/main.b9cc3957.chunk.js"
+  },
+  {
+    "revision": "144b78d646bc6e782816",
+    "url": "/static/js/runtime-main.c1fc542f.js"
+  }
+]);
